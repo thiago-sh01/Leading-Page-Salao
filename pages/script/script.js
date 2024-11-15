932f6675c3b4f99f2c0a152f5a1e61ba2a017161
Thiago Sanches Hohlenwerger <thiagosanchess52@gmail.com>
2024-11-14 21:43:36 -0300

    adiconado na label data, so pode aceitar datas a partir do dia que esta fazendo o preenchimento

--- pages/script/script.js
+++ pages/script/script.js
@@ -27,3 +27,15 @@ function mostrarFormulario() {
 function fecharFormulario() {
   document.getElementById("popupFormulario").classList.remove("active");
 }
+
+function mostrarFormulario() {
+  document.getElementById("popupFormulario").classList.add("active");
+
+  const dataInput = document.getElementById("data");
+  const hoje = new Date();
+  const ano = hoje.getFullYear();
+  const mes = String(hoje.getMonth() + 1).padStart(2, "0");
+  const dia = String(hoje.getDate()).padStart(2, "0");
+
+  dataInput.min = `${ano}-${mes}-${dia}`;
+}
