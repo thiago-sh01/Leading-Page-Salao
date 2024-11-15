5d542f6c0fb4f14b3818b01d26fc68f8e5583952
Thiago Sanches Hohlenwerger <thiagosanchess52@gmail.com>
2024-11-14 21:39:47 -0300

    retirado o popup de confirmação e adicionado de formulário

--- pages/script/script.js
+++ pages/script/script.js
@@ -1,29 +1,29 @@
 function loadHTML(file, elementId) {
-    fetch(file)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`Erro ao carregar ${file}: ${response.status}`);
-            }
-            return response.text();
-        })
-        .then(html => {
-            document.getElementById(elementId).innerHTML = html;
-        })
-        .catch(error => console.error(error));
+  fetch(file)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Erro ao carregar ${file}: ${response.status}`);
+      }
+      return response.text();
+    })
+    .then((html) => {
+      document.getElementById(elementId).innerHTML = html;
+    })
+    .catch((error) => console.error(error));
 }
 
-loadHTML('/pages/header.html', 'header');
-loadHTML('/pages/main.html', 'main');
-loadHTML('/pages/footer.html', 'footer');
+loadHTML("/pages/header.html", "header");
+loadHTML("/pages/main.html", "main");
+loadHTML("/pages/footer.html", "footer");
 
-window.addEventListener('load', function() {
-    document.querySelector('.content').classList.add('active');
+window.addEventListener("load", function () {
+  document.querySelector(".content").classList.add("active");
 });
 
-function mostrarMensagem() {
-    document.getElementById("popup").classList.add("active");
+function mostrarFormulario() {
+  document.getElementById("popupFormulario").classList.add("active");
 }
 
-function fecharMensagem() {
-    document.getElementById("popup").classList.remove("active");
+function fecharFormulario() {
+  document.getElementById("popupFormulario").classList.remove("active");
 }
