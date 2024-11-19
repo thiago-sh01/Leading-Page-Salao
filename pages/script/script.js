c74c8a406dfb8d34963f82d91161b4b77cb6688f
Thiago Sanches Hohlenwerger <thiagosanchess52@gmail.com>
2024-11-19 18:54:14 -0300

    ajustado os carrosseis

--- pages/script/script.js
+++ pages/script/script.js
@@ -79,22 +79,44 @@ function fecharFormulario() {
   document.getElementById("popupFormulario").classList.remove("active");
 }
 
-let slideIndex = 0;
+let slideIndexCabeleireira = 0;
+let slideIndexManicure = 0;
 
-function mudarSlide(n) {
-  const slides = document.querySelectorAll(".slide");
+function mudarSlide(n, tipo) {
+  let slides;
+  let slideIndex;
+
+  if (tipo === "cabeleireira") {
+    slides = document.querySelectorAll(".cabeleireira .slide");
+    slideIndex = slideIndexCabeleireira;
+  } else if (tipo === "manicure") {
+    slides = document.querySelectorAll(".manicure .slide");
+    slideIndex = slideIndexManicure;
+  }
+
   slideIndex += n;
 
   if (slideIndex < 0) {
     slideIndex = slides.length - 1;
   } else if (slideIndex >= slides.length) {
-    slideIndex = 0; 
+    slideIndex = 0;
   }
 
   const offset = -slideIndex * 100;
-  document.querySelector(".slides").style.transform = `translateX(${offset}%)`;
+  if (tipo === "cabeleireira") {
+    document.querySelector(
+      ".cabeleireira .slides"
+    ).style.transform = `translateX(${offset}%)`;
+    slideIndexCabeleireira = slideIndex;
+  } else if (tipo === "manicure") {
+    document.querySelector(
+      ".manicure .slides"
+    ).style.transform = `translateX(${offset}%)`;
+    slideIndexManicure = slideIndex;
+  }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  mudarSlide(0);
+  mudarSlide(0, "cabeleireira");
+  mudarSlide(0, "manicure");
 });
