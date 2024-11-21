9d5ade55617792dbd0bc9462baa48de9f1e4de83
Thiago Sanches Hohlenwerger <thiagosanchess52@gmail.com>
2024-11-21 10:58:50 -0300

    resolvido o problema de ir para page serviços

--- pages/script/script.js
+++ pages/script/script.js
@@ -15,10 +15,15 @@ function loadHTML(file, elementId) {
 loadHTML("/pages/header.html", "header");
 loadHTML("/pages/main.html", "main");
 loadHTML("/pages/footer.html", "footer");
+loadHTML("/pages/servicos.html", "section")
 
 window.addEventListener("load", function () {
   document.querySelector(".content").classList.add("active");
 });
+
+function irParaServicos(){
+  window.location.href = "/pages/servicos.html";
+}
 
 function mostrarFormulario() {
   document.getElementById("popupFormulario").classList.add("active");
